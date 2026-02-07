b94eba2ecf4daf9bf8403d411e7163ae8bae1095
MRennnie <makagonova.renata@yandex.ru>
2026-02-07 14:27:45 +0700

    Update 01_data_overview.sql

--- sql/01_data_overview.sql
+++ sql/01_data_overview.sql
@@ -1,14 +1,41 @@
--- dataset size and diversity
+-- checking data for duplicates
 SELECT
-      COUNT(*) AS total_users,
-      COUNT(DISTINCT country) AS countries,
-      COUNT(DISTINCT device_type) AS device_types
-FROM users_behavior;
-
--- users by device type
-SELECT
-      device_type,
-      COUNT(*) AS users_count
+    COUNT(*) AS total_rows,
+    COUNT(DISTINCT user_id) AS unique_users
 FROM users_behavior
-GROUP BY device_type
-ORDER BY users_count;
+
+-- сhecking for NULL values in key metrics
+SELECT
+    COUNT(*) FILTER (WHERE monthly_spend IS NULL) AS null_monthly_spend,
+    COUNT(*) FILTER (WHERE average_order_value IS NULL) AS null_aov,
+    COUNT(*) FILTER (WHERE purchase_conversion_rate IS NULL) AS null_conversion_rate,
+    COUNT(*) FILTER (WHERE cart_abandonment_rate IS NULL) AS null_cart_abandonment
+FROM users_behavior
+
+-- сhecking the ranges of numeric metrics (validity of values)
+SELECT
+    MIN(age) AS min_age,
+    MAX(age) AS max_age,
+    MIN(monthly_spend) AS min_monthly_spend,
+    MAX(monthly_spend) AS max_monthly_spend,
+    MIN(purchase_conversion_rate) AS min_conversion_rate,
+    MAX(purchase_conversion_rate) AS max_conversion_rate
+FROM users_behavior
+
+-- users without purchases but with non-zero conversion
+SELECT COUNT(*) AS inconsistent_users
+FROM users_behavior
+WHERE weekly_purchases = 0
+  AND purchase_conversion_rate > 0
+      
+-- finding outliers in costs
+ SELECT
+    user_id,
+    monthly_spend
+FROM users_behavior
+WHERE monthly_spend >
+      (SELECT PERCENTILE_CONT(0.99) 
+       WITHIN GROUP (ORDER BY monthly_spend)
+       FROM users_behavior)
+ORDER BY monthly_spend DESC
+LIMIT 20
